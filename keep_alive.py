6a25e701181af606c4296e12de7f7a02879b40d6
Upierczi <>
2021-06-19 01:37:37 +0000

    Added logging, improved behavior, added commands

--- keep_alive.py
+++ keep_alive.py
@@ -1,15 +1,91 @@
-from flask import Flask
+from flask import Flask, Response
 from threading import Thread
+from replit import db
+import json
 
 app = Flask('')
+app.config['JSON_AS_ASCII'] = False
+
+
+# Defines a class for each message in the db
+class Message:
+    id = 0
+    message = ""
+
+    def __init__(self, id, message):
+        self.id = id
+        self.message = message
+
 
 @app.route('/')
 def home():
-  return "Hello. I am alive!"
+    return "Hello. I am alive!"
+
+
+@app.route('/stats')
+def stats():
+    if "encouragements" in db.keys():
+        stats = {
+            'total_messages': len(db["encouragements"]),
+            'total_actions': len(db["actions"])
+        }
+        statsJson = json.dumps(stats,
+                               indent=4,
+                               sort_keys=True,
+                               ensure_ascii=False)
+        return Response(statsJson,
+                        200,
+                        content_type="application/json; charset=utf-8")
+    else:
+        return str(0)
+
+
+# Displays a list of all messages
+@app.route('/bot/messages', methods=['GET'])
+def messages():
+    response = Response(messagesToJson(),
+                        200,
+                        content_type="application/json; charset=utf-8")
+    return response
+
+
+@app.route('/bot/actions', methods=['GET'])
+def actions():
+    response = Response(db["actions"],
+                        200,
+                        content_type="application/json; charset=utf-8")
+    return response
+
 
 def run():
-  app.run(host='0.0.0.0',port=8080)
+    app.run(host='0.0.0.0', port=8080)
+
 
 def keep_alive():
-  t = Thread(target=run)
-  t.start()  
+    t = Thread(target=run)
+    t.start()
+
+
+def messagesToObject():
+    if "encouragements" in db.keys():
+        i = 0
+        encouragements = []
+        for key in db["encouragements"]:
+            encouragements.append(Message(i, key))
+            i += 1
+        return encouragements
+
+
+# Retrieves messages from the db
+def messagesToJson():
+    if "encouragements" in db.keys():
+        i = 0
+        encouragements = []
+        for key in messagesToObject():
+            encouragements.append(key.__dict__)
+            i += 1
+        return json.dumps(encouragements,
+                          indent=4,
+                          sort_keys=True,
+                          ensure_ascii=False)
+    return ''
--- main.py
+++ main.py
@@ -3,93 +3,154 @@ import os
 import requests
 import json
 import random
+import time
 from replit import db
 from keep_alive import keep_alive
 
+last_action_time = int(time.time())
+cooldown = random.randint(5,
+                          random.randint(10, int(time.time() / 100000000 - 1)))
+
+
 def update_encouragements(encouraging_message):
-  if "encouragements" in db.keys():
-    encouragements = db["encouragements"] 
-    encouragements.append(encouraging_message)
-    db["encouragements"] = encouragements
-  else:
-    db["encouragements"] = [encouraging_message]
+    if "encouragements" in db.keys():
+        encouragements = db["encouragements"]
+        encouragements.append(encouraging_message)
+        db["encouragements"] = encouragements
+    else:
+        db["encouragements"] = [encouraging_message]
+
 
 def delete_encouragement(index):
-  encouragements = db["encouragements"]
-  if len(encouragements) > index:
-    del encouragements[index]
-    db["encouragements"] = encouragements  
+    encouragements = db["encouragements"]
+    if len(encouragements) > index:
+        del encouragements[index]
+        db["encouragements"] = encouragements
+
+
+def log_action(action):
+    print(action)
+    if "actions" in db.keys():
+        actions = db["actions"]
+        actions.append(json.dumps({"action": action}))
+        db["actions"] = actions
+    else:
+        db["actions"] = [action]
+
+
+def is_action_too_soon():
+    if (last_action_time + cooldown >= int(time.time())):
+        return True
+    return False
 
 
 client = discord.Client()
 
-sad_words = ["sad", "depressed", "unhappy", "miserable", "depressing"]
-
-if "responding" not in db.keys():
-  db["responding"] = True
-
-starter_encouragements = ["Cheer up!", "Hang in there.", "You are beautiful person with a charming personality!"
+sad_words = [
+    "sad", "depressed", "unhappy", "miserable", "depressing", "magkaras",
+    "malakas", "θλιψη", "thlipsi", "θλίψη", "tsiab", "σαδ"
 ]
 
+if "responding" not in db.keys():
+    db["responding"] = True
+
+starter_encouragements = ["o magaras einai t-word"]
+
+
 def get_quote():
-  response = requests.get("https://zenquotes.io/api/random")
-  json_data = json.loads(response.text)
-  quote = json_data[0]['q'] + " -" + json_data[0]['a']
-  return(quote)
+    response = requests.get("https://zenquotes.io/api/random")
+    json_data = json.loads(response.text)
+    quote = json_data[0]['q'] + " -" + json_data[0]['a']
+    return (quote)
+
 
 @client.event
 async def on_ready():
-  print('Logged in as {0.user}'.format(client))
+    print('Logged in as {0.user}'.format(client))
+
+
+def update_last_action():
+    global last_action_time
+    last_action_time = int(time.time())
+
 
 @client.event
 async def on_message(message):
-  if message.author == client.user:
-    return
+    if message.author == client.user:
+        return
 
-  msg = message.content
+    msg = message.content
 
-  if message.content.startswith('$inspire'):
-    quote = get_quote()
-    await message.channel.send(quote)
+    if message.content.startswith('$inspire'):
+        quote = get_quote()
+        await message.channel.send(quote)
 
-  if db["responding"]:
-    options = starter_encouragements
-    if "encouragements" in db.keys():
-      options = options + db["encouragements"]
+    if db["responding"]:
+        options = starter_encouragements
+        if "encouragements" in db.keys():
+            options = options + db["encouragements"]
 
-    if any(word in msg for word in sad_words):
-      await message.channel.send(random.choice(options))
-  
-  if msg.startswith("$new"):
-    encouraging_message = msg.split("$new ",1)[1]
-    update_encouragements(encouraging_message)
-    await message.channel.send("New encouraging message added!")
-  
-  if msg.startswith("$del"):
-    encouragements = []
-    if "encouragements" in db.keys():
-      index = int(msg.split("$del",1)[1])
-      delete_encouragement(index)
-      encouragements = db["encouragements"]
-    await message.channel.send(encouragements) 
+        if any(word in msg for word in sad_words):
+            await message.channel.send(random.choice(options))
+            if (random.randint(1, 100) > 69):
+              await message.channel.send(random.choice(options))
+              if (random.randint(1, 100) > 98):
+                await message.channel.send(random.choice(options))
 
+    if msg.startswith("8ball"):
+      has_question = msg.split("8ball ", 1)
+      
+      predictions = ["Δεν έχω ιδέα μαγκάρα.", "Σίγουρα θα γίνει.", "Πιθανό αλλά δεν το βλέπω.", "Καλύτερα μην σε πω τώρα...", "Συγκεντρόσου και ρώτισε ξανά.", "Αρκετά πιθανό να γίνει.", "Εγώ λέω όχι.", "Δεν έχω ιδέα.", "Που να ξέρω;", "Όπως βλέπω τα πράγματα τώρα, ποντάρω στο ότι θα γίνει.", "Sorry δεν πρόσεχα ξαναρώτα λίγο.", "Γράψε 'sad' γιατί αυτό βλέπω να γίνεται."]
 
-  if msg.startswith("$list"):
-    encouragements = []
-    if "encouragements" in db.keys():
-      encouragements = db["encouragements"]
-    await message.channel.send(encouragements)
+      if (is_action_too_soon()):
+          await message.channel.send("Μην βιάζεσαι μαγκάρα.")
+      else:
+          update_last_action()
+          if (len(has_question) == 1):
+            await message.channel.send("Ρώτα κάτι μαγκάρα αλλιώς τι να προβλέπω;")
+          else:
+            await message.channel.send(random.choice(predictions))
 
-  if msg.startswith("$responding"):
-    value = msg.split("$responding ", 1)[1]
+    if msg.startswith("$new"):
+        encouraging_message = msg.split("$new ", 1)[1]
+        ts = time.ctime(int(time.time()))
+        log_action("{}: New message incoming from @{} in channel #{}".format(
+            ts, message.author, message.channel))
 
-    if value.lower() == "true":
-      db["responding"] = True
-      await message.channel.send("Responding is on.")
-    else:
-      db["responding"] = False
-      await message.channel.send("Responding is off.")
+        if (is_action_too_soon()):
+            await message.channel.send("Μην βιάζεσαι μαγκάρα.")
+        else:
+            if (len(encouraging_message) < 2):
+                await message.channel.send("Γράψε κάτι παραπάνω, κουφάλα.")
+            else:
+                update_last_action()
+                update_encouragements(encouraging_message)
+                log_action(
+                    "{}: New message added by @{} in channel #{}".format(
+                        ts, message.author, message.channel))
+                await message.channel.send(
+                    "Προστέθηκε νέο ενθαρρυντικό μήνυμα!")
+
+    if msg.startswith("$del"):
+        if "encouragements" in db.keys():
+            index = int(msg.split("$del", 1)[1])
+            delete_encouragement(index)
+        ts = time.ctime(int(time.time()))
+        log_action("{}: Message {} deleted by @{} in channel #{}".format(
+            ts, index, message.author, message.channel))
+        await message.channel.send(
+            "Message with id {} has been deleted!".format(index))
+
+    if msg.startswith("$responding"):
+        value = msg.split("$responding ", 1)[1]
+
+        if value.lower() == "true":
+            db["responding"] = True
+            await message.channel.send("Responding is on.")
+        else:
+            db["responding"] = False
+            await message.channel.send("Responding is off.")
+
 
 keep_alive()
 client.run(os.getenv('TOKEN'))
-
